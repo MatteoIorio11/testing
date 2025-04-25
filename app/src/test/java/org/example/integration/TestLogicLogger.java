dba2c58995d430050bc1295c4c66d39b6e67ab8d
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-04-25 16:21:44 +0200

    test: check logs when game is over

--- app/src/test/java/org/example/integration/TestLogicLogger.java
+++ app/src/test/java/org/example/integration/TestLogicLogger.java
@@ -38,6 +38,7 @@ public class TestLogicLogger {
         verify(logger, atLeast(2)).info(anyString());
         verify(logger, times(1)).info("Hit at position: " + pos1);
         verify(logger, times(1)).info("Check if the game is over");
+        verify(logger, times(1)).info("Add the hitted position");
     }
 
     @Test
@@ -47,5 +48,25 @@ public class TestLogicLogger {
         when(pos1.y()).thenReturn(1);
         assertTrue(logic.getMark(pos1).isEmpty());
         verify(logger, times(1)).info("Get mark for position: " + pos1);
+    }
+
+    @Test
+    public void testPrintsWhenGameIsOver() {
+        final var pos1 = Mockito.mock(org.example.utils.Position.class);
+        final var pos2 = Mockito.mock(org.example.utils.Position.class);
+        final var pos3 = Mockito.mock(org.example.utils.Position.class);
+        when(pos1.x()).thenReturn(0);
+        when(pos2.x()).thenReturn(1);
+        when(pos3.x()).thenReturn(2);
+        when(pos1.y()).thenReturn(0);
+        when(pos2.y()).thenReturn(1);
+        when(pos3.y()).thenReturn(2);
+        assertFalse(logic.hit(pos1).isEmpty());
+        assertTrue(logic.hit(pos2).isEmpty());
+        verify(logger, atLeast(4)).info(anyString());
+        verify(logger, times(1)).info("Moving positions");
+        verify(logger, times(1)).info("Checking if " + pos1 + " and " + pos2 + " are neighbours");
+        assertTrue(logic.hit(pos3).isEmpty());
+        verify(logger, times(1)).info("The Game is Over");
     }
 }
