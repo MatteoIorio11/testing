a3e4c4012fe121755a464dcd1c824d80f4b6806f
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-04-25 15:41:21 +0200

    feat: add implementation for Logger using Log4j

--- app/src/main/java/org/example/logic/LoggerImpl.java
+++ app/src/main/java/org/example/logic/LoggerImpl.java
@@ -1,4 +1,18 @@
 package org.example.logic;
 
-public class LoggerImpl {
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class LoggerImpl implements MyLogger {
+    private final static Logger logger = LogManager.getLogger(LoggerImpl.class);
+    @Override
+    public void info(String message) {
+        logger.info(message);
+    }
+
+    @Override
+    public void error(String message) {
+        logger.error(message);
+    }
 }
