b15e814d850ef2b2b009168daac9979c55a8dba3
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-04-25 16:06:48 +0200

    test: init integration tests, test logs when logic's hit

---
+++ app/src/test/java/org/example/integration/TestLogicLogger.java
@@ -0,0 +1,34 @@
+package org.example.integration;
+
+import org.example.logic.LoggerImpl;
+import org.example.logic.Logic;
+import org.example.logic.LogicImpl;
+import org.example.logic.MyLogger;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertFalse;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+public class TestLogicLogger {
+    private static final int SIZE = 10;
+    private static Logic logic;
+    final MyLogger logger = Mockito.spy(LoggerImpl.class);
+
+    @Before
+    public void initLogic() {
+        logic = new LogicImpl(SIZE, logger);
+    }
+
+    @Test
+    public void testHitPrintsLogs() {
+        final var pos1 = Mockito.mock(org.example.utils.Position.class);
+        when(pos1.x()).thenReturn(1);
+        when(pos1.y()).thenReturn(1);
+        assertFalse(logic.hit(pos1).isEmpty());
+        verify(logger, atLeast(2)).info(anyString());
+        verify(logger, times(1)).info("Hit at position: " + pos1);
+    }
+}
