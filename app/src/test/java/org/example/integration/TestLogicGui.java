5c2aaa7a5c854dfbe2c5887531910c972715e694
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-04-26 10:56:29 +0200

    test: init setUp

--- app/src/test/java/org/example/integration/TestLogicGui.java
+++ app/src/test/java/org/example/integration/TestLogicGui.java
@@ -1,4 +1,26 @@
 package org.example.integration;
 
+import org.example.GUI;
+import org.example.logic.Logic;
+import org.example.logic.LogicImpl;
+import org.example.logic.MyLogger;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+
 public class TestLogicGui {
+    private final static int SIZE = 10;
+    private GUI gui;
+    private final MyLogger mockLogger = Mockito.mock(MyLogger.class);
+    private final Logic spyLogic = Mockito.spy(new LogicImpl(SIZE, mockLogger));
+    @BeforeEach
+    public void setUp() {
+        gui = new GUI(SIZE);
+        gui.setVisible(false);
+        doNothing().when(mockLogger).info(anyString());
+        doNothing().when(mockLogger).error(anyString());
+    }
+
 }
