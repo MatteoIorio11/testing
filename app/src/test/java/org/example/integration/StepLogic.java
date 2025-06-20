46b0ea2ffebdc8d7a0f84b5c887535ea8e56930a
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-20 19:07:21 +0200

    feat: refactorise all the commands

--- app/src/test/java/org/example/integration/StepLogic.java
+++ app/src/test/java/org/example/integration/StepLogic.java
@@ -14,21 +14,50 @@ import static org.junit.Assert.*;
 import java.util.Optional;
 
 public class StepLogic {
-
+    private Logic logic;
+    private Optional<Integer> lastOutput;
+    final MyLogger logger = new LoggerImpl();
 
     public StepLogic() {
+        this.logic = new LogicImpl(10, logger);
+    }
+
+    private void clearAndCreate() {
+        this.logic = new LogicImpl(10, logger);
     }
 
 
-    @Given("The board is empty")
     @When("The user hits the position at x=0, y=0")
     @Then("The returned value should be one")
     public void theReturnedValueShouldBeOne() {
-        final MyLogger logger = new LoggerImpl();
-        final Logic logic = new LogicImpl(10, logger);
-        final Optional<Integer> output = logic.hit(new Position(0, 0));
-        assertFalse(output.isEmpty());
-        final int value = output.get();
+        this.clearAndCreate();
+        this.lastOutput = this.logic.hit(new Position(0, 0));
+        assertFalse(this.lastOutput.isEmpty());
+        final int value = this.lastOutput.get();
         assertEquals(1, value);
+    }
+
+    @When("The user hits position x={int}, y={int} and x={int}, y={int}")
+    public void theUserHitsPositionXYAndXY(int arg0, int arg1, int arg2, int arg3) {
+        this.clearAndCreate();
+        final Position position = new Position(arg0, arg1);
+        final Position position2 = new Position(arg2, arg3);
+        this.logic.hit(position);
+        this.lastOutput = this.logic.hit(position2);
+    }
+
+    @Then("The returned value should be two")
+    public void theReturnedValueShouldBeTwo() {
+        System.out.println(this.lastOutput);
+        if (this.lastOutput.isEmpty()) {
+            fail();
+        }
+        final int value = this.lastOutput.get();
+        assertEquals(2, value);
+    }
+
+    @Given("The board is empty")
+    public void theBoardIsEmpty() {
+        this.clearAndCreate();
     }
 }
