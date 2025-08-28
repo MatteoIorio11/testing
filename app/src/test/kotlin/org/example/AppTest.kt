cd430ffb2a148a06ca2fdebfa375180acdd1fb97
matteo.iorio <matteo.iorio01@gmail.com>
2025-08-28 10:58:50 +0200

    fix: flaky test

--- app/src/test/kotlin/org/example/AppTest.kt
+++ app/src/test/kotlin/org/example/AppTest.kt
@@ -18,7 +18,7 @@ class AppTest {
 
     @Test fun randoFail() {
         val randomNumber = Random.nextInt()
-        assertEquals(randomNumber, randomNumber + 1)
+        assertEquals(randomNumber, randomNumber)
     }
 
     @Test fun alwaysTrue() {
